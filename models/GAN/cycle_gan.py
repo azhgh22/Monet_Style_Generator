89858476b19d06ee59e795b8ffe1b2ffb05769cc
azhgh22 <azhgh22@freeuni.edu.ge>
2025-12-10 12:42:28 +0000

    ignore

--- models/GAN/cycle_gan.py
+++ models/GAN/cycle_gan.py
@@ -2,14 +2,27 @@ import torch
 import torch.nn as nn
 
 class CycleGAN(nn.Module):
-  def __init__(self,y_gen, x_gen, y_desc, x_desc) -> None:
+  def __init__(self,
+        y_gen, x_gen, y_desc, x_desc,
+        generator_optimizer,
+        y_desc_optimizer,
+        x_desc_optimizer,
+        gan_criterion,
+        consistency_criterion,
+        identiry_criterion 
+      ) -> None:
     super().__init__()
 
     self.y_gen = y_gen
     self.x_gen = x_gen
     self.y_desc = y_desc
     self.x_desc = x_desc
-
+    self.generator_optimizer = generator_optimizer
+    self.y_desc_optimizer = y_desc_optimizer
+    self.x_desc_optimizer = x_desc_optimizer
+    self.gan_criterion = gan_criterion
+    self.consistency_criterion = consistency_criterion
+    
 
   def forward(self, x, y):
     """
@@ -23,3 +36,11 @@ class CycleGAN(nn.Module):
     rec_y = self.y_gen(fake_x)
 
     return fake_y, fake_x, rec_y, rec_x
+
+
+  def train_step(self, batch):
+    x, y = batch[0], batch[1]
+    gen_y, gen_x, rec_y, rec_x = self(x,y)
+
+    #  L_Gan loss = 
+
