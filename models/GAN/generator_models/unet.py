2a3d43b9a10438692b4f787aa84e04af57646c77
azhgh22 <azhgh22@freeuni.edu.ge>
2025-12-21 07:48:15 +0000

    Unet

--- models/GAN/generator_models/unet.py
+++ models/GAN/generator_models/unet.py
@@ -7,42 +7,43 @@ class UNetGeneratorCUT(nn.Module):
 
         # ---------- Encoder ----------
         self.enc1 = self.conv_block(in_ch, base)
-        self.enc2 = self.conv_block(base, base*2)
-        self.enc3 = self.conv_block(base*2, base*4)
-        self.enc4 = self.conv_block(base*4, base*8)
+        self.enc2 = self.conv_block(base, base * 2)
+        self.enc3 = self.conv_block(base * 2, base * 4)
+        self.enc4 = self.conv_block(base * 4, base * 8)
 
         self.pool = nn.AvgPool2d(2)
 
         # ---------- Bottleneck ----------
-        self.bottleneck = self.conv_block(base*8, base*8)
+        self.bottleneck = self.conv_block(base * 8, base * 8)
 
         # ---------- Decoder ----------
-        self.up4 = self.up_block(base*8, base*8)
-        self.dec4 = self.conv_block(base*16, base*4)
+        self.up4 = self.up_block(base * 8, base * 8)
+        self.dec4 = self.conv_block(base * 16, base * 4)
 
-        self.up3 = self.up_block(base*4, base*4)
-        self.dec3 = self.conv_block(base*8, base*2)
+        self.up3 = self.up_block(base * 4, base * 4)
+        self.dec3 = self.conv_block(base * 8, base * 2)
 
-        self.up2 = self.up_block(base*2, base*2)
-        self.dec2 = self.conv_block(base*4, base)
+        self.up2 = self.up_block(base * 2, base * 2)
+        self.dec2 = self.conv_block(base * 4, base)
 
         self.up1 = self.up_block(base, base)
-        self.dec1 = self.conv_block(base*2, base)
+        self.dec1 = self.conv_block(base * 2, base)
 
         self.out = nn.Conv2d(base, out_ch, kernel_size=1)
 
+    # ---------- Blocks ----------
     def conv_block(self, in_c, out_c):
         return nn.Sequential(
-            nn.Conv2d(in_c, out_c, 3, padding=1),
+            nn.Conv2d(in_c, out_c, kernel_size=3, padding=1),
             nn.InstanceNorm2d(out_c),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_c, out_c, 3, padding=1),
+            nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
             nn.InstanceNorm2d(out_c),
             nn.ReLU(inplace=True),
         )
 
     def up_block(self, in_c, out_c):
-        return nn.ConvTranspose2d(in_c, out_c, 2, stride=2)
+        return nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2)
 
     # ---------- CUT requirement ----------
     def encode(self, x):
@@ -50,10 +51,13 @@ class UNetGeneratorCUT(nn.Module):
 
         e1 = self.enc1(x)
         feats.append(e1)
+
         e2 = self.enc2(self.pool(e1))
         feats.append(e2)
+
         e3 = self.enc3(self.pool(e2))
         feats.append(e3)
+
         e4 = self.enc4(self.pool(e3))
         feats.append(e4)
 
